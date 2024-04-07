a514ee11deff808c887c69f99057d923e7bfd485
ThalesMilanezi <milanezitf@gmail.com>
2024-04-07 12:28:02 -0300

    chore: initial setup with material ui

---
+++ src/components/components.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+
+import { MaterialUiModule } from "../app/material-ui/material-ui.module";
+
+@NgModule({
+  declarations: [],
+  imports:[MaterialUiModule],
+  exports:[]
+})
+export class ComponentsModule {
+
+}
